402b770fd62f09057a7013a1a7abdfa63c349450
Vicki Yang <vickiwyang@gmail.com>
2021-05-17 15:14:45 -0500

    boilerplate ts demo app using atomic-kafka

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const AtomicKafka = require('atomic-kafka')
+
+const port = 3000;
+
+
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+app.get('/', (req, res) => {
+  console.log('*** serving root of demo app ( / )');
+  res.sendFile(path.resolve(__dirname + '/index.html'))
+})
+
+/**
+ * 404 handler
+ */
+app.use('*', (req, res) => {
+  return res.status(404).send('********** GLOBAL BAD REQUEST / 404 ERROR **********');
+});
+
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+const atomicKafkaInstance = new AtomicKafka(server);
+atomicKafkaInstance.newConsumer('truck-group');
+atomicKafkaInstance.socketConsume('truck-group', 'test_topic');
+atomicKafkaInstance.newProducer('test_topic');
+atomicKafkaInstance.socketProduce('test_topic');
