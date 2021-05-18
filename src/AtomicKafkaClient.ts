a53c6314216332f1ece6e97bccc4b18b33ba0805
Nikhil Massand <nikhilmassand@Nikhils-MBP.cable.rcn.com>
2021-05-17 23:42:50 -0400

    added components and other minor changes

---
+++ src/AtomicKafkaClient.ts
@@ -0,0 +1,36 @@
+import io from "socket.io-client";
+
+class AtomicKafkaClient {
+  address: string;
+  constructor(kafkaServer){
+    this.address = kafkaServer;
+    // this.io = require('socket.io-client')(kafkaServer, {
+		// 	cors: {
+		// 		origin: '*',
+		// 	}
+		// });
+  }
+
+  consumer(state, setState){
+    const socket = io(this.address);
+    // const socket = io(this.address);
+    console.log('in clientSocketConsume()');
+    console.log('state in clientSocketConsume(): ', state);
+    // console.log('setState in clientSocketConsume(): ', setState);
+    socket.on("newMessage", (arg) => {
+      console.log("new data: ", arg);
+      // console.log("data type: ", typeof arg);
+      console.log("new truck state: ", state);
+      // if(arg.SKU === state[state.length - 1].SKU) return;
+      return setState([...state, arg]);
+    });
+    return () => {
+      console.log("is App ever off?");
+      socket.off();
+    }
+  }
+}
+
+
+
+export default AtomicKafkaClient;
